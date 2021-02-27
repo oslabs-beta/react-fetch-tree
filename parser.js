3cc067c7b8e96cb9ec573946f63e8053a9c2f0a7
Cara Dibdin <cdibdin@gmail.com>
2021-02-27 16:37:34 -0500

    updated parser, fiberwalker

--- parser.js
+++ parser.js
@@ -6,16 +6,16 @@ const path = require("path");
 let ID = 0;
 const cache = {};
 
-//Obtain  target file's dependencies 
+//Obtain  target file's dependencies
 const getDependencies = (filename) => {
   //Declare dataRequestObject
   const dataRequests = [];
 
   class DataRequestObject {
     constructor(dataRequestType, position, variableName) {
-      this.dataRequestType = dataRequestType
-      this.position =  position || null
-      this.variableName = variableName || 'Anonymous'
+      this.dataRequestType = dataRequestType;
+      this.position = position || null;
+      this.variableName = variableName || "Anonymous";
     }
   }
   //Read file content
@@ -33,71 +33,72 @@ const getDependencies = (filename) => {
   //Traverse AST using babeltraverse to identify imported nodes
   traverse(raw_ast, {
     ImportDeclaration: ({ node }) => {
-      if (node.source.value.indexOf('./') !== -1) dependencies.push(node.source.value);
+      if (node.source.value.indexOf("./") !== -1)
+        dependencies.push(node.source.value);
     },
     Function(path) {
       const IdentifierPath = {
-        CallExpression: ({node}) => {
-          if (node.callee.name === 'fetch') {
-            const dataRequest = new DataRequestObject('fetch');
-            dataRequests.push(dataRequest);
-          };
-        },
-      MemberExpression: ({ node }) => {
-        if (node.object.name === 'axios') {
-          let exists = false;
-          dataRequests.forEach(existingDataRequest => {
-            if (existingDataRequest.position === node.loc.start) {
-              exists = true;
-            }
-          })
-          if (!exists) {
-            const dataRequest = new DataRequestObject('axios', node.loc.start);
+        CallExpression: ({ node }) => {
+          if (node.callee.name === "fetch") {
+            const dataRequest = new DataRequestObject("fetch");
             dataRequests.push(dataRequest);
           }
-        };
-        if (node.property.name === 'ajax') {
-          const dataRequest = new DataRequestObject('ajax');
-          dataRequests.push(dataRequest);
-        };
-        if (node.object.name === 'http') {
-          const dataRequest = new DataRequestObject('http');
-          dataRequests.push(dataRequest);
-        };
-        if (node.object.name === 'https') {
-          const dataRequest = new DataRequestObject('https');
-          dataRequests.push(dataRequest);
-        };
-        if (node.object.name === 'qwest') {
-          const dataRequest = new DataRequestObject('qwest');
-          dataRequests.push(dataRequest);
-        };
-        if (node.object.name === 'superagent') {
-          const dataRequest = new DataRequestObject('superagent');
-          dataRequests.push(dataRequest);
-        };
-      },
-      NewExpression: ({ node }) => {
-        if (node.callee.name === 'XMLHttpRequest') {
-          const dataRequest = new DataRequestObject('XMLHttpRequest');
-          dataRequests.push(dataRequest);
-        };
-      },
-    }
-        path.traverse(IdentifierPath)
+        },
+        MemberExpression: ({ node }) => {
+          if (node.object.name === "axios") {
+            let exists = false;
+            dataRequests.forEach((existingDataRequest) => {
+              if (existingDataRequest.position === node.loc.start) {
+                exists = true;
+              }
+            });
+            if (!exists) {
+              const dataRequest = new DataRequestObject(
+                "axios",
+                node.loc.start
+              );
+              dataRequests.push(dataRequest);
+            }
+          }
+          if (node.property.name === "ajax") {
+            const dataRequest = new DataRequestObject("ajax");
+            dataRequests.push(dataRequest);
+          }
+          if (node.object.name === "http") {
+            const dataRequest = new DataRequestObject("http");
+            dataRequests.push(dataRequest);
+          }
+          if (node.object.name === "https") {
+            const dataRequest = new DataRequestObject("https");
+            dataRequests.push(dataRequest);
+          }
+          if (node.object.name === "qwest") {
+            const dataRequest = new DataRequestObject("qwest");
+            dataRequests.push(dataRequest);
+          }
+          if (node.object.name === "superagent") {
+            const dataRequest = new DataRequestObject("superagent");
+            dataRequests.push(dataRequest);
+          }
+        },
+        NewExpression: ({ node }) => {
+          if (node.callee.name === "XMLHttpRequest") {
+            const dataRequest = new DataRequestObject("XMLHttpRequest");
+            dataRequests.push(dataRequest);
+          }
+        },
+      };
+      path.traverse(IdentifierPath);
     },
 
     enter(path) {
       //Check data request type
-​
       // if (path.isIdentifier({ name: "fetch" })) {
       //   const dataRequest = new DataRequestObject('fetch');
       //   dataRequests.push(dataRequest);
       // }
-​
-    }
-  })
-
+    },
+  });
   const id = ID++;
   cache[filename] = id;
 
@@ -105,7 +106,7 @@ const getDependencies = (filename) => {
     id,
     filename,
     dependencies,
-    dataRequests
+    dataRequests,
   };
 };
 
@@ -117,16 +118,16 @@ const dependenciesGraph = (entryFile) => {
     asset.mapping = {};
     const dirname = path.dirname(asset.filename);
 
-    asset.dependencies.forEach(relativePath => {
+    asset.dependencies.forEach((relativePath) => {
       //If there is no file extension, add it
       let absolutePath = path.resolve(dirname, relativePath);
-      let fileCheck = fs.existsSync(absolutePath)
+      let fileCheck = fs.existsSync(absolutePath);
       let child;
 
       if (!fileCheck) {
-        absolutePath = path.resolve(dirname, relativePath + '.js'); //Test for .js
+        absolutePath = path.resolve(dirname, relativePath + ".js"); //Test for .js
         fileCheck = fs.existsSync(absolutePath);
-        if (!fileCheck) absolutePath = absolutePath + 'x'; //Test for .jsx
+        if (!fileCheck) absolutePath = absolutePath + "x"; //Test for .jsx
       }
 
       //Check for duplicate file paths
@@ -135,13 +136,13 @@ const dependenciesGraph = (entryFile) => {
         queue.push(child);
       }
       asset.mapping[relativePath] = cache[absolutePath];
-    })
+    });
   }
   // console.log(queue[0].dataRequests)
   // console.log(queue[1].dataRequests)
   // console.log(queue[2].dataRequests)
   return queue;
-}
+};
 
-console.log(dependenciesGraph('./src/index.js'));
+console.log(dependenciesGraph("./src/index.js"));
 console.log(cache);
---
+++ src/debug.js
@@ -0,0 +1,9 @@
+const componentObj = {
+  FetchTree: {},
+  ProfilePage: {
+    "line: 25, column: 8": { reqType: "fetch", parentName: "fetchUser" },
+  },
+  ProfileTimeline: {
+    "line: 36, column: 9": { reqType: "fetch", parentName: "fetchPosts" },
+  },
+};
