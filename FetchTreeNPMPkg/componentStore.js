c894419304f5f451cf7e2a6f0a9f33cdd99dd930
Anika <munikamustafiz89@gmail.com>
2021-04-01 22:52:31 -0400

    add css to html file

--- FetchTreeNPMPkg/componentStore.js
+++ FetchTreeNPMPkg/componentStore.js
@@ -1,2 +1,3 @@
 const componentObj = {"Fetchtree":{},"ProfilePage":{},"ProfileTimeline":{},"ClassTest":{},"null":{"line: 27, column: 2":{"reqType":"fetch","parentName":null},"line: 28, column: 2":{"reqType":"axios","parentName":null},"line: 2, column: 0":{"reqType":"fetch","parentName":null},"line: 26, column: 8":{"reqType":"fetch","parentName":null},"line: 37, column: 9":{"reqType":"fetch","parentName":null}},"CreateClassTest":{}}
 module.exports = componentObj;
+
