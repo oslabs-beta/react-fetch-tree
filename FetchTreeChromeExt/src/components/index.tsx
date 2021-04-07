cd39b308f4333f9416a0207410dfea4e62951e57
James Ferrell <jamesferrell@jamess-mbp.lan>
2021-04-06 20:54:17 -0400

    updated treeviz styling

--- FetchTreeChromeExt/src/components/index.tsx
+++ FetchTreeChromeExt/src/components/index.tsx
@@ -4,27 +4,7 @@ import ParentSize from '@visx/responsive/lib/components/ParentSize';
 import Viz from './treeViz';
 import './sandbox-styles.css';
 
-// import React from "react";
-// import ReactDOM from "react-dom";
-// import ParentSize from '@visx/responsive/lib/components/ParentSize';
-// import Viz from "./treeViz.js";
-// //import componentStore from "@reactfetchtree/rft/componentStore";
-
-// //let that = componentStore;
-// function ProfileTimeline() {
-//   return (
-//     <div>
-//       <p style={{ color: "white" }}>This element </p>
-//       <Viz />
-//     </div>
-//   );
-// }
-
-// ReactDOM.render(<ProfileTimeline />, document.getElementById("app"));
-
-
-
-  render(
-    <ParentSize>{({ width, height }) => <Viz width={width} height={height * 0.9} />}</ParentSize>,
-    document.getElementById('app'),
-  );
+render(
+  <ParentSize>{({ width, height }) => <Viz width={width} height={height * 0.9} />}</ParentSize>,
+  document.getElementById('app'),
+);
