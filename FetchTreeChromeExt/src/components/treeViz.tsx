52b23011fca3b4687ce766bc0a1b42573c7db8b3
Cara Dibdin <cdibdin@gmail.com>
2021-04-10 13:15:35 -0400

    Add messaging

--- FetchTreeChromeExt/src/components/treeViz.tsx
+++ FetchTreeChromeExt/src/components/treeViz.tsx
@@ -1,4 +1,164 @@
-import React, { useDebugValue, useState } from "react";
+// import React, { useMemo, useState, useEffect } from "react";
+// import { Group } from "@visx/group";
+// import { Cluster, hierarchy } from "@visx/hierarchy";
+// import { LinkVertical } from "@visx/shape";
+// import { LinearGradient } from "@visx/gradient";
+
+// const citrus = "#ddf163";
+// const white = "#ffffff";
+// const green = "#79d259";
+// const aqua = "#37ac8c";
+// const merlinsbeard = "#f7f7f3";
+// const background = "#306c90";
+
+// function Node({ node }) {
+//   const isRoot = node.depth === 0;
+//   const isParent = !!node.children;
+//   const width = node.data.name.length * 4.5 + 10;
+//   const height = 20;
+//   const centerX = -width / 2;
+//   const centerY = -height / 2;
+//   if (isRoot) return <RootNode node={node} />;
+//   console.log("name", node.data.name.length);
+//   return (
+//     <Group top={node.y} left={node.x}>
+//       {node.depth !== 0 && (
+//         <rect
+//           width={width}
+//           height={height}
+//           x={-width / 2}
+//           y={-height / 2}
+//           fill={background}
+//           stroke={isParent ? white : citrus}
+//           onClick={() => {
+//             alert(`clicked: ${JSON.stringify(node.data.name)}`);
+//           }}
+//         />
+//       )}
+//       <text
+//         dy=".33em"
+//         fontSize={9}
+//         fontFamily="Arial"
+//         textAnchor="middle"
+//         style={{ pointerEvents: "none" }}
+//         fill={isParent ? white : citrus}
+//       >
+//         {node.data.name}
+//       </text>
+//     </Group>
+//   );
+// }
+
+// function RootNode({ node }) {
+//   const width = 50;
+//   const height = 20;
+//   const centerX = -width / 2;
+//   const centerY = -height / 2;
+
+//   return (
+//     <Group top={node.y} left={node.x}>
+//       <rect
+//         width={width}
+//         height={height}
+//         y={centerY}
+//         x={centerX}
+//         fill="url('#top')"
+//       />
+//       <text
+//         dy=".33em"
+//         fontSize={9}
+//         fontFamily="Arial"
+//         textAnchor="middle"
+//         style={{ pointerEvents: "none" }}
+//         fill={background}
+//       >
+//         {node.data.name}
+//       </text>
+//     </Group>
+//   );
+// }
+
+// const defaultMargin = { top: 40, left: 10, right: 10, bottom: 40 };
+
+// function Example({ width, height, margin = defaultMargin }) {
+//   const [orgChart, setOrgChart] = useState({ name: "App", children: [] });
+
+//   const port = chrome.runtime.connect({ name: "React Fetch Tree" });
+
+//   // establishes a connection between devtools and background page
+//   port.postMessage({
+//     name: "connect",
+//     tabId: chrome.devtools.inspectedWindow.tabId,
+//   });
+
+//   // Listens for posts sent in specific ports and redraws tree
+//   port.onMessage.addListener((message) => {
+//     // if (!message.data) return; // abort if data not present, or if not of type object
+//     // if (typeof msg !== 'object') return;
+//     // curData = msg; // assign global data object
+//     // throttledDraw();
+//     console.log("in tree viz", message);
+//     setOrgChart(message.payload);
+//   });
+
+//   // console.log(orgChart);
+
+//   //USING useMemo
+//   const data = useMemo(() => {
+//     console.log("orgChart", orgChart);
+//     return hierarchy(orgChart);
+//   }, [orgChart]);
+
+//   //Using useEffect
+//   // var data = hierarchy(orgChart);
+//   // useEffect(() => {
+//   //   console.log("useEffect fired", orgChart);
+//   //   data = hierarchy(orgChart);
+//   // }, [orgChart]);
+
+//   const xMax = width - margin.left - margin.right - 20;
+//   const yMax = height - margin.top - margin.bottom;
+
+//   return width < 10 ? null : (
+//     <svg width={width} height={height}>
+//       <LinearGradient id="top" from={green} to={aqua} />
+//       <rect width={width} height={height} rx={14} fill={background} />
+//       <Cluster root={data} size={[xMax, yMax]}>
+//         {(cluster) => (
+//           <Group top={margin.top} left={margin.left}>
+//             {cluster.links().map((link, i) => (
+//               <LinkVertical
+//                 key={`cluster-link-${i}`}
+//                 data={link}
+//                 stroke={merlinsbeard}
+//                 strokeWidth="1"
+//                 strokeOpacity={0.2}
+//                 fill="none"
+//               />
+//             ))}
+//             {cluster.descendants().map((node, i) => (
+//               <Node key={`cluster-node-${i}`} node={node} />
+//             ))}
+//           </Group>
+//         )}
+//       </Cluster>
+//     </svg>
+//   );
+// }
+
+// export default function Viz() {
+//   return (
+//     <div className="Viz" style={{ display: "flex" }}>
+//       <Example
+//         width={500}
+//         height={400}
+//         style={{ width: "500px", height: "300px" }}
+//       />
+//     </div>
+//   );
+// }
+
+import React, { useDebugValue, useState, useMemo } from "react";
 import { Group } from "@visx/group";
 import { hierarchy, Tree } from "@visx/hierarchy";
 import { LinearGradient } from "@visx/gradient";
@@ -7,7 +167,6 @@ import LinkControls from "./LinkControls";
 import getLinkComponent from "./getLinkComponent";
 import { Zoom } from "@visx/zoom";
 import { localPoint } from "@visx/event";
-
 
 interface TreeNode {
   name: string;
@@ -21,47 +180,48 @@ interface DataRequest {
   dataRequest: string;
 }
 
-const data: TreeNode = {
-  name: "T",
-  children: [
-    {
-      name: "App",
-      dataRequest: "fetch",
-      children: [
-        { name: "Component 1" },
-        { name: "Component 2" },
-        { name: "This is a very long name component 3" },
-        {
-          name: "Im outraged",
-          children: [
-            {
-              name: "C1",
-            },
-            {
-              name: "D",
-              children: [
-                {
-                  name: "D1",
-                },
-                {
-                  name: "D2",
-                },
-                {
-                  name: "D3",
-                },
-              ],
-            },
-          ],
-        },
-      ],
-    },
-    { name: "Z" },
-    {
-      name: "B",
-      children: [{ name: "B1" }, { name: "B2" }, { name: "B3" }],
-    },
-  ],
-};
+//const data: TreeNode = orgChart;
+// {
+//   name: "T",
+//   children: [
+//     {
+//       name: "App",
+//       dataRequest: "fetch",
+//       children: [
+//         { name: "Component 1" },
+//         { name: "Component 2" },
+//         { name: "This is a very long name component 3" },
+//         {
+//           name: "Im outraged",
+//           children: [
+//             {
+//               name: "C1",
+//             },
+//             {
+//               name: "D",
+//               children: [
+//                 {
+//                   name: "D1",
+//                 },
+//                 {
+//                   name: "D2",
+//                 },
+//                 {
+//                   name: "D3",
+//                 },
+//               ],
+//             },
+//           ],
+//         },
+//       ],
+//     },
+//     { name: "Z" },
+//     {
+//       name: "B",
+//       children: [{ name: "B1" }, { name: "B2" }, { name: "B3" }],
+//     },
+//   ],
+// };
 
 const defaultMargin = { top: 30, left: 30, right: 30, bottom: 70 };
 
@@ -83,9 +243,31 @@ export default function Viz({
   const forceUpdate = useForceUpdate();
   const [displayFetch, setDisplayFetch] = useState<boolean>(false);
   const [fetchComponent, setFetchComponent] = useState<DataRequest>({name: "", dataRequest: ""})
+  const [orgChart, setOrgChart] = useState<TreeNode>({name:"App"});
+const port = chrome.runtime.connect({ name: "React Fetch Tree" });
 
+    // establishes a connection between devtools and background page
+    port.postMessage({
+      name: "connect",
+      tabId: chrome.devtools.inspectedWindow.tabId,
+    });
+
+    // Listens for posts sent in specific ports and redraws tree
+    port.onMessage.addListener((message) => {
+      // if (!message.data) return; // abort if data not present, or if not of type object
+      // if (typeof msg !== 'object') return;
+      // curData = msg; // assign global data object
+      // throttledDraw();
+      console.log("in tree vis", message);
+      setOrgChart(message.payload);
+    });
   const innerWidth = totalWidth - margin.left - margin.right;
   const innerHeight = totalHeight - margin.top - margin.bottom;
+
+  const data: TreeNode = useMemo(() => {
+    console.log("orgChart", orgChart);
+    return orgChart;
+  }, [orgChart]);
 
   let origin: { x: number; y: number };
   let sizeWidth: number;
@@ -167,9 +349,9 @@ export default function Viz({
                     zoom.scale({ scaleX: 1.1, scaleY: 1.1, point });
                   }}
                 />
-                
+
                 <Tree
-                  root ={hierarchy(data, (d) =>
+                  root ={hierarchy(orgChart, (d) =>
                     d.isExpanded ? null : d.children
                   )}
                   size={[sizeWidth, sizeHeight]}
@@ -192,7 +374,7 @@ export default function Viz({
 
                       {tree.descendants().map((node, key) => {
                         const width = 60;
-                        const height = 40;
+                        const height = 30;
 
                         let top: number;
                         let left: number;
@@ -218,14 +400,14 @@ export default function Viz({
                               />
                             )}
                             {node.depth !== 0 && (
-                              
+
                               <rect
-                                height={30}
-                                width={node.data.name.length>5 ? node.data.name.length * 5.5 : 25}
-                                
-                                y={-height / 2.8}
-                                x={node.data.name.length>12 ? -width*1.6 : node.data.name.length<5 ? -width/4.5 : -width / 2}
-                                fill={
+                              height={30}
+                              width={node.data.name.length<4 ? 30 : node.data.name.length>15 ? node.data.name.length*4.5 : node.data.name.length * 6}
+                              
+                              y={-height / 2}
+                              x={node.data.name.length<4 ? -15 : node.data.name.length>15 ? -node.data.name.length*2.25 : -node.data.name.length * 3}
+                              fill={
                                   node.data.dataRequest ? "yellow" : "#272b4d"
                                 }
                                 stroke={
@@ -237,7 +419,7 @@ export default function Viz({
                                 }
                                 strokeOpacity={node.data.children ? 1 : 0.6}
                                 rx={node.data.children ? 0 : 10}
-                              
+
                                 onClick={() => {
                                   console.log("clicked");
                                   if (node.data.dataRequest) {
@@ -290,7 +472,7 @@ export default function Viz({
                     </Group>
                   )}
                 </Tree>
-               
+
               </Group>
             </svg>
             <div className="controls">
@@ -341,4 +523,3 @@ export default function Viz({
     </div>
   );
 }
-
