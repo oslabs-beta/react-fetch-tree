26ce227fb45465a0c42a5f2aa9c96e7eccfba810
Chris Lung <chrislung@Chriss-MacBook-Pro.local>
2021-04-10 12:47:59 -0400

    Added more styling to ComponentStorePanel

--- FetchTreeChromeExt/src/components/ComponentStorePanel.jsx
+++ FetchTreeChromeExt/src/components/ComponentStorePanel.jsx
@@ -11,11 +11,11 @@ const ComponentStorePanel = () => {
           <div key={`component${i}`} className='component'> 
             <span className='componentName'> { key[0] !== "null" && key[0] } </span>
             <div className='componentDetails'> 
-              <span className='details'>{ key[0] !== "null" && Object.entries(key[1]).length === 0 && 'No Data Requests In Component' }</span>
+              <span className='details'>{ key[0] !== "null" && Object.entries(key[1]).length === 0 && '"No data requests"' }</span>
             </div>
             {Object.values(key[1]).map(key => 
               <div key={`${key.reqType}${key.parentName}${i}`} class='componentDetails'> 
-                <span className='detailsLabel'>Data Request Type:</span> <span className='details'>{ key.reqType }</span>, <span className='detailsLabel'>Parent:</span> <span className='details'>{ key.parentName }</span> 
+                <span className='detailsLabel'>Data Request Type:</span> <span className='details'>"{ key.reqType }"</span>, <span className='detailsLabel'>Parent:</span> <span className='details'>"{ key.parentName }"</span> 
               </div>
             )}
           </div>
