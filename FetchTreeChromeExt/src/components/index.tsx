0e66bacee585313958128621a604a71b002757da
Chris Lung <chrislung@Chriss-MacBook-Pro.local>
2021-04-10 14:58:12 -0400

    Removed TestHook from index.jsx
    Co-authored by: Trevor Carr <trevor.a.carr@gmail.com>
    Co-authored by: Anika Mustafiz <munikamustafiz89@gmail.com>

--- FetchTreeChromeExt/src/components/index.tsx
+++ FetchTreeChromeExt/src/components/index.tsx
@@ -2,12 +2,10 @@ import React from "react";
 import { render } from "react-dom";
 import "./sandbox-styles.css";
 import Panel from "./Panel";
-import TestHook from "./testHook";
 
 render(
   <div>
     <Panel />
-    <TestHook />
   </div>,
   document.getElementById("app")
 );
