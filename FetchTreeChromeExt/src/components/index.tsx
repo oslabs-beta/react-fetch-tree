87405e782288ffbba57281882ccdb589262870c2
Chris Lung <chrislung@Chriss-MacBook-Pro.local>
2021-04-10 14:51:51 -0400

    Added messaging functionality to Panel.jsx
    Co-authored by: Trevor Carr <trevor.a.carr@gmail.com>
    Co-authored by: Anika Mustafiz <munikamustafiz89@gmail.com>

--- FetchTreeChromeExt/src/components/index.tsx
+++ FetchTreeChromeExt/src/components/index.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
-import { render } from 'react-dom';
-import './sandbox-styles.css';
+import React from "react";
+import { render } from "react-dom";
+import "./sandbox-styles.css";
 import Panel from "./Panel";
+import TestHook from "./testHook";
 
 render(
   <div>
     <Panel />
+    <TestHook />
   </div>,
-  document.getElementById('app'),
+  document.getElementById("app")
 );
