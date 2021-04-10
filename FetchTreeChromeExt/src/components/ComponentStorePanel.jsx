674215f8ac78fc25d0aff3aa005a27f323f7057a
Chris Lung <chrislung@Chriss-MacBook-Pro.local>
2021-04-10 13:16:45 -0400

    Adding coauthor
    Co-authored-by: James Ferrell <jdferrell0909@gmail.com>

--- FetchTreeChromeExt/src/components/ComponentStorePanel.jsx
+++ FetchTreeChromeExt/src/components/ComponentStorePanel.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const componentStore = JSON.parse('{"App":{},"null":{},"Nav":{"line: 6, column: 4":{"reqType":"axios","parentName":"Nav"}},"Body":{"line: 7, column: 4":{"reqType":"fetch","parentName":"Body"},"line: 1, column: 26":{"reqType":"fetch","parentName":"testVarExp"}},"Footer":{"line: 11, column: 2":{"reqType":"fetch","parentName":"Footer"},"line: 21, column: 9":{"reqType":"fetch","parentName":"testArrowExp"}}}')
 const componentArr = Object.entries(componentStore);
-console.log(componentArr);
+// console.log(componentArr);
 
 const ComponentStorePanel = () => {
   return (
