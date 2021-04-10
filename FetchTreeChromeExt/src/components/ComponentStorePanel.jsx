4c346590955122ed0372ab3f2e73346ce80f8356
Chris Lung <chrislung@Chriss-MacBook-Pro.local>
2021-04-10 15:00:37 -0400

    Co-Authoring
    Co-authored by: Trevor Carr <trevor.a.carr@gmail.com>
    Co-authored by: Anika Mustafiz <munikamustafiz89@gmail.com>

--- FetchTreeChromeExt/src/components/ComponentStorePanel.jsx
+++ FetchTreeChromeExt/src/components/ComponentStorePanel.jsx
@@ -9,6 +9,7 @@ const ComponentStorePanel = (props) => {
             <div className='componentDetails'> 
               <span className='details'>{ key[0] !== "null" && Object.entries(key[1]).length === 0 && '"No data requests"' }</span>
             </div>
+            
             {Object.values(key[1]).map(key => 
               <div key={`${key.reqType}${key.parentName}${i}`} className='componentDetails'> 
                 <span className='detailsLabel'>Data Request Type:</span> <span className='details'>"{ key.reqType }"</span> <span className='detailsLabel'>Parent:</span> <span className='details'>"{ key.parentName }"</span> 
