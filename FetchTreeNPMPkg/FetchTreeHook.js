6196c773969552502d3aa62f120ebe08ba3e0d16
Trevor Carr <trevor.a.carr@gmail.com>
2021-04-15 15:20:57 -0400

    refactor FetchTreeHook to be class component

--- FetchTreeNPMPkg/FetchTreeHook.js
+++ FetchTreeNPMPkg/FetchTreeHook.js
@@ -1,16 +1,25 @@
 import componentObj from './componentStore';
-import { useState } from 'react';
+import { Component } from 'react';
 
 //Declare FetchTreeHook that user will import into their codebase
-const FetchTreeHook = () => {
-  const [dummy, setDummy] = useState(true);
-  //Send componentObj to devtools panel 
-  window.postMessage({ type: 'componentObj', payload: componentObj }, '*');
-  //Trigger state change to populate data in panel
-  setTimeout(() => {
-    setDummy(!dummy);
-  }, 2000);
-  return null;
-};
+class FetchTreeHook extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      dummy: true,
+    };
+  }
+
+  render() {
+    window.postMessage({ type: 'componentObj', payload: componentObj }, "*");
+    //Trigger state change to populate data in panel
+    setTimeout(() => {
+      this.setState({
+        dummy: true,
+      });
+    }, 2000);
+    return null;
+  }
+}
 
 export default FetchTreeHook;
